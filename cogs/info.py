c1d79c2e978aaae2d2deb802dfb35d20ae02d2ec
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-11-22 14:21:08 +0530

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -13,7 +13,7 @@ class InfoCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    guild_id = 1028619109044326440
+    guild_id = 1040237301814546462
 
     @nextcord.slash_command(name="info", description="To access the information of a particular recipe", guild_ids=[guild_id])
     async def info(self, interaction, id: int):
