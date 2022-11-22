ef04491c53cf5fa3037112384d871612a04cbec1
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-11-22 14:20:22 +0530

    Update cost.py

--- cogs/cost.py
+++ cogs/cost.py
@@ -9,7 +9,7 @@ class CostCog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    guild_id = 1028619109044326440
+    guild_id = 1040237301814546462
 
     @nextcord.slash_command(name="cost", description="To access the cost value of a particular recipe", guild_ids=[guild_id])
     async def cost(self, interaction, id: int):
